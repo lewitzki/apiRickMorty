d51d96740a447e6a401672d766a624830225c355
lewitzki <matozopedro8@gmail.com>
2024-10-09 16:58:46 -0300

    calculadora

--- script2.js
+++ script2.js
@@ -1,37 +1,27 @@
-//exercicio 1
-const idade=20;
-if (idade >= 20) {
-    console.log("voce é maior");
-} else {
-    console.log("voce é de menor");
-}
+function calculadora = (a, b, operacao) 
+    if (operacao === "soma") 
+        return a + b;
+          
+    else if (operacao === "subtracao")
+        return a - b;
+    
+    else if (operacao === "multiplicacao")
+        return a * b;
+       
+    else if (operacao === "divisao")        
+    if (b !== 0) {
+        return a / b;}
+         
+    else     
+        return "Divisão por zero não é permitida";     
+      
+    else 
+        return ("Operação inválida")
+    
 
-//exercico 2
-let numero = prompt("digite um numero positivo");
-if (numero % 2 == 0){
-    cnsole.log("O numero" + numero,"É PAR");
-}else if (numero % 2 != 0){
-    console.log ("O numero" + numero, "É IMPAR");
-}else{
-    console.log ("O numero precisa ser positivo!")
-}
-
-//exercicio 3
-let numero2 = prompt("Digite um número:");
-if (numero2 > 0) {
-    console.log("Número positivo");
-} 
-else if (numero2< 0){
-console.log("Número negativo");
-} else {
-console.log("Zero");
-}
-
-
-//exercico 4
-for (let i = 1; i < 11; i++) {
-    let total =5 * i;
-    console.log("5 x" + i + " = " + total);
-}  
-
-let contador = 10;
+    return ("Divisão por zero não é permitida");         
+    console.log(calculadora(10, 5, "subtracao"));     
+    console.log(calculadora(10, 5, "multiplicacao"));  
+    console.log(calculadora(10, 5, "divisao"));        
+    console.log(calculadora(10, 0, "divisao"));        
+    console.log(calculadora(10, 5, "invalido"));     
